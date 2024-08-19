30bb915da36205c5c862c9b7f4ea3b3e9545eea6
taowen <taowen@sobey.com>
2024-08-19 19:10:44 +0800

    feat: ts调整

--- src/read.browser.ts
+++ src/read.browser.ts
@@ -1,11 +1,11 @@
 let uid = 0;
 
-export function readJsonpData(fileUrl: string) {
+export function readJsonpData<T = any>(fileUrl: string): Promise<T> {
   const exportFuncName: string = `_jsonp_data_read${uid++}_`;
   const fileURLObject = new URL(fileUrl);
-  fileURLObject.searchParams.append('exportFunc', exportFuncName);
+  fileURLObject.searchParams.append("exportFunc", exportFuncName);
   return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     (window as any)[exportFuncName] = function (data: string) {
       const dataStr = window.atob(data);
       document.body.removeChild(script);
