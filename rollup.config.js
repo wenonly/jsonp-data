b908e57b1f5291b5712f9d04d17e798d60ec2c12
taowen <taowen@sobey.com>
2024-08-20 18:27:04 +0800

    feat: 更改打包方式

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,31 @@
 const typescript = require("@rollup/plugin-typescript");
 
-module.exports = {
-  input: "src/read.browser.ts",
-  output: {
-    file: "lib/jsonp-data.browser.js",
-    format: "umd",
-    name: "JsonpData",
+module.exports = [
+  {
+    input: "src/read.browser.ts",
+    output: {
+      file: "lib/jsonp-data.browser.js",
+      format: "umd",
+      name: "JsonpData",
+    },
+    plugins: [
+      typescript({
+        declaration: false,
+      }),
+    ],
   },
-  plugins: [
-    typescript({
-      declaration: false,
-    }),
-  ],
-};
+  {
+    input: "src/index.ts",
+    output: {
+      file: "lib/index.js",
+      format: "umd",
+      name: "JsonpData",
+    },
+    plugins: [
+      typescript({
+        declaration: true,
+        declarationDir: "lib",
+      }),
+    ],
+  },
+];
